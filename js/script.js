f5563b31d8baf968241cc8bf261b660a585cd59b
Daniyel <daniyel_github-1@mail.ru>
2022-02-16 22:30:49 +0300

    added func fingMinValueInArr

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+
+// Given an array of integers your solution should find the smallest integer.
+
+const arr = [-5, 78, 56, 1, 232, 12, 8, -10];
+
+const fingMinValueInArr = (args) => {
+	// const value = args.reduce((prev, item) => {
+	// 	if (prev > item) {
+	// 		return item;
+	// 	}
+	// 	else {
+	// 		return prev;
+	// 	}
+	// });
+	// return value;
+	const value = args.reduce((prev, item) => {
+		return prev > item ? item : prev;
+	});
+	return value;
+};
+
+console.log(fingMinValueInArr(arr));
