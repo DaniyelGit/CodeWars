8d9bb7e9f2d7137f7abc79cb759517d7c85a633e
Daniyel Tkachuk <daniyel_github-1@mail.ru>
2022-02-20 22:56:36 +0300

    added func findCountVowels

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
-'use strict';
-
+'use strict'
 
 // ----------------- ***Given an array of integers your solution should find the smallest integer.*** ----------
 
-const arr = [-5, 78, 56, 1, 232, 12, 8, -10];
+const arr = [-5, 78, 56, 1, 232, 12, 8, -10]
 
-const fingMinValueInArr = (args) => {
+const fingMinValueInArr = args => {
 	// const value = args.reduce((prev, item) => {
 	// 	if (prev > item) {
 	// 		return item;
@@ -16,36 +15,56 @@ const fingMinValueInArr = (args) => {
 	// });
 	// return value;
 	const value = args.reduce((prev, item) => {
-		return prev > item ? item : prev;
-	});
-	return value;
-};
+		return prev > item ? item : prev
+	})
+	return value
+}
 
-console.log(fingMinValueInArr(arr));
-
+console.log(fingMinValueInArr(arr))
 
 //  ----------------------------------- ***Remove String Spaces*** --------------------------------
 
-const string = '8 j 8   mBliB8g  imjB8B8   jl  B';
+const string = '8 j 8   mBliB8g  imjB8B8   jl  B'
 
-const removeStringSpaces = (str) => {
+const removeStringSpaces = str => {
 	// return str.split(' ').join('');
-	return str.replace(/\s/g, '');
-};
+	return str.replace(/\s/g, '')
+}
 
-console.log(removeStringSpaces(string));
-
+console.log(removeStringSpaces(string))
 
 // ----------------------------- ***Count sheep in array*** -----------------------------
 
-const arrayOfSheep = [true, true, true, false,
-	true, true, true, true,
-	true, false, true, false,
-	true, false, false, true,
-	true, true, true, true,
-	false, false, true, true, true, true];
+const arrayOfSheep = [
+	true,
+	true,
+	true,
+	false,
+	true,
+	true,
+	true,
+	true,
+	true,
+	false,
+	true,
+	false,
+	true,
+	false,
+	false,
+	true,
+	true,
+	true,
+	true,
+	true,
+	false,
+	false,
+	true,
+	true,
+	true,
+	true,
+]
 
-const countSheepInArr = (arr) => {
+const countSheepInArr = arr => {
 	// let result = 0;
 	// for (let i = 0; i < arr.length; i++) {
 	// 	if (arr[i]) {
@@ -60,21 +79,53 @@ const countSheepInArr = (arr) => {
 
 	// return result;
 
-	return arr.filter(s => s).length;
-};
+	return arr.filter(s => s).length
+}
 
-console.log(countSheepInArr(arrayOfSheep));
-
+console.log(countSheepInArr(arrayOfSheep))
 
 // ---------------- ***We need a function that can transform a number into a string.*** -------------------
 
-const number = 67;
+const number = 67
 
 function numberToString(num) {
-	return String(num);
+	return String(num)
 	// return num + '';
 	// return num.toString();
-
 }
 
-console.log(numberToString(number));
+console.log(numberToString(number))
+
+// ------------------- ***Return the number (count) of vowels in the given string.***------------------------------
+
+const strForVowels = 'abracadabra'
+
+const findCountVowels = str => {
+	const vowels = ['a', 'e', 'i', 'o', 'u']
+	// let count = 0
+
+	// for (let char of str.toLowerCase()) {
+	// 	if (vowels.includes(char)) {
+	// 		count += 1
+	// 	}
+	// }
+	// return count
+
+	const result = str
+		.toLowerCase()
+		.split('')
+		.reduce((prev, item) => {
+			if (vowels.includes(item)) {
+				return (prev += 1)
+			} else {
+				return prev
+			}
+		}, 0)
+
+	return result
+
+	// let vowels = str.match(/[aeiou]/gi)
+	// return vowels ? vowels.length : 0
+}
+
+console.log(findCountVowels(strForVowels))
